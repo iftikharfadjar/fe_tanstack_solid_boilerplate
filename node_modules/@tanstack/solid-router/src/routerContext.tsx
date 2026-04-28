10fdad51a2d2a4c2d06770a93744929093bb23e7
FAINA <fadjar.iftikhar@gmail.com>
2026-04-28 15:15:01 +0700

    add gitignore

--- node_modules/@tanstack/solid-router/src/routerContext.tsx
+++
@@ -1,6 +0,0 @@
-import * as Solid from 'solid-js'
-import type { AnyRouter } from '@tanstack/router-core'
-
-export const routerContext = Solid.createContext<AnyRouter>(
-  null as unknown as AnyRouter,
-)
